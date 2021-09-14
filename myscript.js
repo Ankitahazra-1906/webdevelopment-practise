4bfd811d558851425042c21d6701373975503a54
Ankita Hazra <ankitahazra2000@gmail.com>
2021-09-14 21:24:14 +0530

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,14 @@
+/*function myfunc(){
+    
+    document.getElementById("demo").innerHTML= 52+4;
+}*/
+function myfunc2(){
+let a,b,c;
+a=6;
+b=12;
+c=  (a*b)+a;
+c+= " is the calculation"
+document.getElementById("demo2").innerHTML=c;
+document.getElementById("demo3").innerHTML= "hello everyone";
+document.getElementById("demo4").innerHTML= "Ankita Hazra is learning javascript";
+}
